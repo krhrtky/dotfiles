c73d83841a89f0562d4da73667c1b394948015cf
takuya.kurihara <takuya.kurihara00@gmail.com>
2025-07-03 08:33:59 +0900

    simplify

--- config/nvim/lua/plugins/code.lua
+++ config/nvim/lua/plugins/code.lua
@@ -502,42 +502,6 @@ return {
     end,
   },
   
-  -- Terminal Integration
-  {
-    'akinsho/toggleterm.nvim',
-    version = '*',
-    keys = {
-      { '<C-\\>', '<cmd>ToggleTerm<cr>', desc = 'Toggle Terminal' },
-      { '<leader>th', '<cmd>ToggleTerm size=10 direction=horizontal<cr>', desc = 'Toggle Horizontal Terminal' },
-      { '<leader>tv', '<cmd>ToggleTerm size=80 direction=vertical<cr>', desc = 'Toggle Vertical Terminal' },
-      { '<leader>tf', '<cmd>ToggleTerm direction=float<cr>', desc = 'Toggle Floating Terminal' },
-    },
-    config = function()
-      require('toggleterm').setup({
-        size = 20,
-        open_mapping = [[<c-\>]],
-        hide_numbers = true,
-        shade_filetypes = {},
-        shade_terminals = true,
-        shading_factor = 2,
-        start_in_insert = true,
-        insert_mappings = true,
-        persist_size = true,
-        direction = 'float',
-        close_on_exit = true,
-        shell = vim.o.shell,
-        float_opts = {
-          border = 'curved',
-          winblend = 0,
-          highlights = {
-            border = 'Normal',
-            background = 'Normal',
-          },
-        },
-      })
-    end,
-  },
-  
   -- Test Framework
   {
     'nvim-neotest/neotest',
